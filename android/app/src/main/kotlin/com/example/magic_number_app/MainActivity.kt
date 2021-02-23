1bfccc58a3a7f268c9078581aabc93da956f418c
Andre Posman <andre.posman@gmail.com>
2021-02-23 00:33:57 -0300

    initial commit and splash_screen

---
+++ android/app/src/main/kotlin/com/example/magic_number_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.magic_number_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text('Magic Number',
+            style: TextStyle(fontSize: 22.0, color: Colors.black)),
+      ),
+    );
+  }
+}
